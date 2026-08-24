bdd55b81442426b4816d6d26a4b570399aa12993
satvika-02 <satvikagelli@gmail.com>
2026-08-24 18:07:48 +0530

    Add files via upload

---
+++ append a file.py
@@ -0,0 +1,7 @@
+file = open("student.txt", "a")
+
+file.write("\nGrade: A")
+
+file.close()
+
+print("Data appended successfully")
---
+++ write a file.py
@@ -0,0 +1,9 @@
+file = open("student.txt", "w")
+
+file.write("Name: Satvika\n")
+file.write("Course: Python\n")
+file.write("Marks: 85")
+
+file.close()
+
+print("Data written successfully")
