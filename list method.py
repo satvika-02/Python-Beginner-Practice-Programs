c76a4502b798a3dba5ddff711b9503cae220700e
satvika-02 <satvikagelli@gmail.com>
2026-08-24 18:46:23 +0530

    Add files via upload

---
+++ list method.py
@@ -0,0 +1,27 @@
+students = ["satvi", "manju", "lahari"]
+
+print("Original List:", students)
+
+# append()
+students.append("satvi")
+print("After append:", students)
+
+# insert()
+students.insert(1, "lahari")
+print("After insert:", students)
+
+# remove()
+students.remove("manju")
+print("After remove:", students)
+
+# pop()
+students.pop()
+print("After pop:", students)
+
+# sort()
+students.sort()
+print("After sort:", students)
+
+# reverse()
+students.reverse()
+print("After reverse:", students)
